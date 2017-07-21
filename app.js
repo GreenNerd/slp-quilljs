589fd9883350108f5964c56eb81aaa619c7fcf5e
moonRider <solar.pom@gmail.com>
2017-07-21 09:30:58 +0800

    import header and remove size

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import Quill from 'quill/core';
 import Toolbar from 'quill/modules/toolbar';
 import Slp from './themes/slp';
 
-import { SizeClass, SizeStyle } from 'quill/formats/size';
+import Header from 'quill/formats/header';
 import { ColorClass, ColorStyle } from 'quill/formats/color';
 import { BackgroundClass, BackgroundStyle } from 'quill/formats/background';
 import Bold from 'quill/formats/bold';
@@ -21,18 +21,15 @@ Quill.register({
   'attributors/class/align': AlignClass,
   'attributors/class/background': BackgroundClass,
   'attributors/class/color': ColorClass,
-  'attributors/class/size': SizeClass,
 
   'attributors/style/align': AlignStyle,
   'attributors/style/background': BackgroundStyle,
   'attributors/style/color': ColorStyle,
-  'attributors/style/size': SizeStyle
 })
 
 Quill.register({
   'modules/toolbar': Toolbar,
   'themes/slp': Slp,
-  'formats/size': SizeClass,
   'formats/color': ColorStyle,
   'formats/background': BackgroundStyle,
   'formats/bold': Bold,
@@ -42,6 +39,7 @@ Quill.register({
   'formats/blockquote': Blockquote,
   'formats/link': ALink,
   'formats/list': List,
+  'formats/header': Header,
   'formats/list/item': ListItem,
   'formats/align': AlignClass,
   'formats/indent': Indent
