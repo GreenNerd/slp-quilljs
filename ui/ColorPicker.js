2203dace4380faada52fea90ff7d33afa377e7ec
moonRider <solar.pom@gmail.com>
2017-07-24 20:24:52 +0800

    add color to label style in color_picker

--- ui/ColorPicker.js
+++ ui/ColorPicker.js
@@ -3,6 +3,7 @@ import Picker from 'quill/ui/picker';
 class ColorPicker extends Picker {
   constructor(select) {
     super(select);
+    this.container.classList.add('ql-color-picker');
     document.body.addEventListener('mousedown', (event)=>{
       if ([].indexOf.call(this.container.classList, 'ql-expanded') === -1) {
         return true;
@@ -27,6 +28,13 @@ class ColorPicker extends Picker {
     let item = super.buildItem(option);
     item.style.backgroundColor = option.getAttribute('value');
     return item;
+  }
+
+  selectItem(item, trigger) {
+    super.selectItem(item, trigger);
+    let value = item.getAttribute('data-value') || 'inherit';
+    this.label.style.color = value;
+    this.label.classList.remove('ql-active');
   }
 }
 
